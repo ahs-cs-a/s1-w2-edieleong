a53bbc68ff9badf7e6ff6e020912f85d607159f1
edieleong <ediemayleong@gmail.com>
2024-08-23 16:57:45 +0000

    changes to bodyofwater

--- BodyOfWater.java
+++ BodyOfWater.java
@@ -1,1 +1,42 @@
+public class BodyOfWater {
+    private String name;
+    private double largestDiameter;
+    private double avgDepth;
+    private boolean isSaltWater;
 
+    public BodyOfWater(String n, double ld, double ad, boolean sw){
+        name = n;
+        largestDiameter = ld;
+        avgDepth = ad;
+        isSaltWater = sw;
+    }
+
+    public BodyOfWater(String n, double ad){
+        name = n;
+        avgDepth = ad;
+    }
+
+    public String name(){
+        return name;
+    }
+
+    public double largestDiameter(){
+        return largestDiameter;
+    }
+
+    public double avgDepth(){
+        return avgDepth;
+    }
+
+    public boolean isSaltWater(){
+        return isSaltWater;
+    }
+
+    public void setName(String n){
+        name = n;
+    }
+
+    public void setLargestDiameter(double ld){
+        largestDiameter = ld;
+    }
+}
--- BodyOfWaterMain.java
+++ BodyOfWaterMain.java
@@ -1,6 +1,22 @@
 public class BodyOfWaterMain {
     public static void main(String[] args) {
-        BodyOfWater b = new BodyOfWater();
-        System.out.println("here");
+        BodyOfWater b = new BodyOfWater("Lake Tahoe", 12, 1645, false);
+        BodyOfWater b1 = new BodyOfWater("San Francisco Bay", 15);
+
+        System.out.println(b.name());
+        System.out.println(b1.name());
+
+        System.out.println(b.largestDiameter());
+        System.out.println(b1.largestDiameter());
+
+        System.out.println(b.avgDepth());
+        System.out.println(b1.avgDepth());
+
+        System.out.println(b.isSaltWater());
+        System.out.println(b1.isSaltWater());
+
+        b1.setName("SF Bay");
+
+        System.out.println(b1.name());
     }
 }
